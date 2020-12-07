845ec5b54fbffe7009194b5824ccd15c6796fe61
ioneliabuzatu <buzatuionelia@gmail.com>
2020-12-07 19:16:26 +0100

    download pretrained weights

---
+++ download_pretrained_weights.sh
@@ -0,0 +1,14 @@
+save_weight_dir="./Yet-Another-EfficientDet-Pytorch/weights/"
+
+mkdir -p $save_weight_dir
+
+wget -O $save_weight_dir/efficientdet-d0.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d0.pth
+wget -O $save_weight_dir/efficientdet-d1.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d1.pth
+wget -O $save_weight_dir/efficientdet-d2.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d2.pth
+wget -O $save_weight_dir/efficientdet-d3.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d3.pth
+wget -O $save_weight_dir/efficientdet-d4.pth https://github.com/zylo117/Yet-Another-mfficient-Pytorch/releases/download/1.0/efficientdet-d4.pth
+wget -O $save_weight_dir/efficientdet-d5.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d5.pth
+wget -O $save_weight_dir/efficientdet-d6.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d6.pth
+wget -O $save_weight_dir/efficientdet-d7.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d7.pth
+wget -O $save_weight_dir/efficientdet-d8.pth https://github.com/zylo117/Yet-Another-Efficient-Pytorch/releases/download/1.0/efficientdet-d8.pth
+
