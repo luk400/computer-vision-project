27e50580b4fa0626b0182e40d1c9598a6be31416
lukas <pichlmannlukas@gmail.com>
2020-12-08 01:22:22 +0100

    also moved acf.m and data_augmentation.m to util

--- util/acf.m
+++ util/acf.m
@@ -1,4 +1,3 @@
-addpath 'util'
 img_folder = './results/';
 trainingsites = { 'F0', 'F1', 'F2', 'F3', 'F5', 'F6', 'F8', 'F9', 'F10', 'F11'}; 
 testsites = { 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8'};
