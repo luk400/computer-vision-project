967c61e7bc7eccc7d5032459caebc794ac2b8f5b
lukas <pichlmannlukas@gmail.com>
2020-12-10 00:32:28 +0100

    can now be executed from util directory

--- util/visualize_dataset.py
+++ util/visualize_dataset.py
@@ -1,5 +1,7 @@
 import cv2
 from torch.utils.data import DataLoader
+import sys
+sys.path.append('../Yet-Another-EfficientDet-Pytorch')
 
 from efficientdet.dataset import CocoDataset as ThermalDataset
 
@@ -7,7 +9,6 @@ if __name__ == "__main__":
     root_dir = "../Yet-Another-EfficientDet-Pytorch/datasets/cv_project"
     training_set = ThermalDataset(root_dir=root_dir, set="train")
     training_generator = DataLoader(training_set)
-
     for data in training_generator:
         img_from_torch_to_numpy = data["img"][0].numpy()
         annotations = data["annot"][0]
@@ -16,3 +17,4 @@ if __name__ == "__main__":
             cv2.rectangle(img_from_torch_to_numpy, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
         cv2.imshow("img", img_from_torch_to_numpy)
         cv2.waitKey(0)
+
