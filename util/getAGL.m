49962d008f13c4b113adfb8d19b6e318b4b5c508
Hari Shankar <43772343+Harighs@users.noreply.github.com>
2021-01-24 20:51:17 +0100

    Update getAGL.m

--- util/getAGL.m
+++ util/getAGL.m
@@ -7,34 +7,34 @@ function agl = getAGL( sitename )
     agl = 37; % per default!
     
     if( strcmpi( sitename, 'F6' ) )
-       agl = 33; 
+       agl = 34; 
     elseif( strcmpi( sitename, 'F5' ) )
-        agl = 30;
+        agl = 32;
     elseif( strcmpi( sitename, 'F0' ) )
         agl = 45;
 	elseif( strcmpi( sitename, 'F1' ) )
-        agl = 30;
+        agl = 28;
     elseif( strcmpi( sitename, 'F2' ) )
-        agl = 38;
+        agl = 32;
     elseif( strcmpi( sitename, 'F3' ) )
-        agl = 40;
+        agl = 41;
     elseif( strcmpi( sitename, 'F4' ) )
         agl = 40;
     elseif( strcmpi( sitename, 'T2' ) )
         agl = 32;
-    elseif( strcmpi( sitename, 'T3' ) )
+    elseif( strcmpi( sitename, 'T3' ) ) % chosen better value not the best one
         agl = 33;
     elseif( strcmpi( sitename, 'T4' ) )
-        agl = 33;
-    elseif( strcmpi( sitename, 'T6' ) )
+        agl = 35;
+    elseif( strcmpi( sitename, 'T6' ) ) % chosen better value not the best one
         agl = 32;
-    elseif( strcmpi( sitename, 'F8' ) ) % this and the following were added by me
+    elseif( strcmpi( sitename, 'F8' ) )
         agl = 24;
     elseif( strcmpi( sitename, 'F9' ) ) 
         agl = 26;
-    elseif( strcmpi( sitename, 'F10' ) ) %unsure about this one 
-        agl = 17;
-    elseif( strcmpi( sitename, 'F11' ) ) %no idea about this one
-        agl = 20;
+    elseif( strcmpi( sitename, 'F10' ) ) 
+        agl = 14;
+    elseif( strcmpi( sitename, 'F11' ) ) 
+        agl = 25;
     end
 end
